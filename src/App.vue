abd4682ef40167113118b5cf7241daf30943055c
JerryYanzhiwei <JerrYanzhiwei@outlook.com>
2021-04-21 18:47:10 +0800

    fixed bug

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <MainNav />
+    <MainNav v-if="deviceType === 1" />
     <transition name="fade"
                 mode="out-in">
       <router-view/>
@@ -13,6 +13,14 @@ import MainNav from '@/components/MainNav.vue'
 export default {
   components: {
     MainNav
+  },
+  data () {
+    return {
+      deviceType: null // 1: PC 2: phone
+    }
+  },
+  mounted () {
+    this.deviceType = document.body.clientWidth > 500 ? 1 : 2
   }
 }
 </script>
